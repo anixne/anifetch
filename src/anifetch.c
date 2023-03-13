47e9c19ddd0731ca23830fece2934cbf5a096857
Kamaye Ryunosuke <112374620+anixne@users.noreply.github.com>
2023-03-13 18:29:48 +0500

    Update anifetch.c

--- src/anifetch.c
+++ src/anifetch.c
@@ -17,7 +17,7 @@ int main(void) {
   printf(YELLOW_CL "Kernel version: %s\n" DEFAULT_CL, sys_info.release);
 
   // hostname
-  char *hostname = (char *)calloc(MAX_LINE_LENGTH, sizeof(char));
+  char *hostname = (char *)calloc(32, sizeof(char));
   if (gethostname(hostname, sizeof(hostname) * MAX_LINE_LENGTH) == -1) {
     print_error("Failed to get hostname");
     return 1;
