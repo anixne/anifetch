f79f876f61aed41bc5b87a2b2c822bb7a03b43a8
nizomovasadbek <nizomovasadbekjava@gmail.com>
2023-06-26 12:49:22 +0500

    Removed return 1

--- src/anifetch.c
+++ src/anifetch.c
@@ -20,17 +20,17 @@ int main(void) {
   char *envVarValue = getenv("XDG_CURRENT_DESKTOP");
   if (envVarValue == NULL) {
     printf("Desktop environment not found\n");
-    return 1;
-  }
+  } else {
   printf("DE: %s\n", envVarValue);
+  }
 
   // hostname
   char *hostname = (char *)calloc(MAX_LINE_LENGTH, sizeof(char));
   if (gethostname(hostname, sizeof(hostname) * 32) == -1) {
     print_error("Failed to get hostname");
-    return 1;
-  }
+  } else {
   printf(BLUE_CL "Hostname: %s\n" DEFAULT_CL, hostname);
+  }
   free(hostname);
 
   // shell
